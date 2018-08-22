506df44771b76fb87ffbc8f2179fe26af12d980c
unknown <tech2js@gmail.com>
2018-08-22 20:52:37 +0800

    created renderlist of bins,as well as remove bin function

--- client/components/App.js
+++ client/components/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Header from './Header';
 import BinsList from './bins/BinsList';
 
-export default () => {
+export default App = () => {
   return (
     <div>
       <Header />
